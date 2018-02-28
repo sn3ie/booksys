6d761581244a0a98781a3cf6ab2d20b352faa27a
sn3ie <35991782+sn3ie@users.noreply.github.com>
2018-02-28 09:47:17 +0900

    登録～検索までok
    ・<%= page_entries_info @shops %>がエラー
    ・ｃｓｓ設定が不十分

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,10 +15,9 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.page(params[:page])
-#    @books = Book.page(params[:page]).per(10).order(:bar_no)
-#    @books = Book.all
-#   @books = Book.where(bar_no: true).page(params[:page])
+#    @books = Book.page(params[:page])
+#   
+    @books = Book.page(params[:page]).search(params[:search]).where.not(bar_no: nil)
   end
  
   # GET /books/1
@@ -76,12 +75,15 @@ class BooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Use callbacks to share common setup or constraints between actions.コールバックを使用して、アクション間で共通の設定や制約を共有します。
+    #特定のIDのデータを取得したい
     def set_book
       @book = Book.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white list through.怖いインターネットからのパラメータを決して信用せず、ホワイトリストだけを許可する。
+    #require メソッドを利用することで、引数に設定した key の 値だけを取得することができます。
+    #permit メソッドは許可したいパラメータだけをフィルタしてくれます。
     def book_params
       params.require(:book).permit(:bar_no, :link_no, :title, :title_yomi, :series, :series_yomi, :volume, :author, :author_yomi, :publisher, :publisher_yomi, :p_ymd, :price, :isbn1_issn, :isbn2, :types, :collection, :label1, :label2, :label3, :status, :lend_date, :back_date, :lend_flg, :user_id, :memo)
     end
--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
+#デフォルトを変更したい項目のみコメントを外せばOK
 Kaminari.configure do |config|
-   config.default_per_page = 25
-   config.max_per_page = nil
-   config.window = 4
-   config.outer_window = 0
-   config.left = 0
-   config.right = 0
-   config.page_method_name = :page
-   config.param_name = :page
-   config.params_on_first_page = false
+  #ページあたりの表示件数
+   config.default_per_page = 10
+  
+  #ページあたりの表示件数の最大
+#   config.max_per_page = nil
+  
+  #現在ページから左右に何ページ分のリンクを生成するかの設定
+#   config.window = 4
+  
+  #PrevとNextから何ページ分リンク生成するかの設定、以下のleft、rightを指定した場合はそちらが優先されます。
+#   config.outer_window = 0
+  
+  #Prevから何ページ分リンクを生成するかの設定
+#   config.left = 0
+  
+  #Nextから何ページ分リンクを生成するかの設定
+#   config.right = 0
+  
+  #モデルに追加されるページ番号を指定するスコープ名
+#   config.page_method_name = :page
+  
+  #ページ番号を渡すためのリクエストパラメーター
+#   config.param_name = :page
+  
+  #
+#   config.params_on_first_page = false
 end
